5f10ae745cc058d9e46c001368be62a78d2241b3
kiko1134 <kristianyordanov156@gmail.com>
2025-01-22 14:20:03 +0200

    Small corrections for better readbility

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -7,48 +7,48 @@ import numpy as np
 file_path = "./data/Sofia_Electricity_Consumption_Updated_2014_2024.csv"
 data = pd.read_csv(file_path)
 
-# Preview the data
+# Провека дали данните се прочитат правилно
 print(data.head())
 
-# Convert the 'Date' column to datetime
+# Конвертираме датата към datetime формат
 data['Date'] = pd.to_datetime(data['Date'])
 
-# Check for missing values
+# Проверка за липсващи стойности
 print("Missing values:\n", data.isnull().sum())
 
-# Handle missing values if any
-data.ffill()  # Forward fill for simplicity
+# При липса на стойности тук се допълват
+data.ffill()  # използваме forward fill за олеснение
 
 
-# Extract time-based features
+# Извличаме времево базираните стойности
 data['Year'] = data['Date'].dt.year
 data['Month'] = data['Date'].dt.month
 
-# Define feature columns and target variable
+# Дефинираме feature колоните и target променливите
 features = ['Year', 'Month', 'Avg_Temperature_C', 'Electricity_Price_BGN_per_MWh', 'Holiday_Period',
             'Population_Thousands']
 target = 'Electricity_Consumption_MWh'
 
-# Split into features (X) and target (y)
+# Разделяме на features (X) и target (y)
 X = data[features]
 y = data[target]
 
 
-# Split the data into training and testing sets
+# Разделяме данните на тренирани (80%) и тестови (20%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train the linear regression model
+# Трениране на модел чрез линейна регресия
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-# Display model coefficients
+# Извеждане в конзолата на коефициентите на модела
 print("Model Coefficients:", model.coef_)
 print("Model Intercept:", model.intercept_)
 
-# Make predictions on the test set
+# Make predictions on the test set Създаване на предсказание на тестовите данни
 y_pred = model.predict(X_test)
 
-# Calculate evaluation metrics
+# Изчисляваме показателите за оценка в случая (MSE,RMSE,r2)
 mse = mean_squared_error(y_test, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_test, y_pred)
@@ -69,4 +69,4 @@ future_data = {
 future_df = pd.DataFrame(future_data)
 future_prediction = model.predict(future_df)
 
-print("Predicted Electricity Consumption for Jan 2025:", future_prediction[0])
+print("Предполагаема консумация на електроенергия за януари 2025:", round(future_prediction[0],2),"MWh")
